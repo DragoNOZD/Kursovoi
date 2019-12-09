033c6b5112e3dd087da3d3bdd1d3508e90877fb2
DragoNOZD <alazar14@yandex.ru>
2019-12-09 12:04:32 +0300

    Пркатика 2.3

--- App/src/main/java/ru/mmo/database/controllers/LoginController.java
+++ App/src/main/java/ru/mmo/database/controllers/LoginController.java
@@ -1,6 +1,5 @@
 package ru.mmo.database.controllers;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
--- App/src/main/java/ru/mmo/database/controllers/UserController.java
+++ App/src/main/java/ru/mmo/database/controllers/UserController.java
@@ -1,6 +1,5 @@
 package ru.mmo.database.controllers;
 
-import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
